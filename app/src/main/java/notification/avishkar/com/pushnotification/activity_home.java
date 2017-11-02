a02f6c8a748bd02ccbf302363f200ee292adb8f3
Rakesh <rakesh.krashna@gmail.com>
2017-11-02 19:31:55 +0530

    Rakesh Parmar

--- app/src/main/java/notification/avishkar/com/pushnotification/activity_home.java
+++ app/src/main/java/notification/avishkar/com/pushnotification/activity_home.java
@@ -35,16 +35,15 @@ public class activity_home extends AppCompatActivity {
     ListView home_Listview;
     DatabaseHelper mydb;
     String title, message, insured, email, phone, policy_num, amount, notes, currency, due_date;
-    String[] titles_str = {"My Notifications        	>", "My Calendar        	 	>", "Key Business Tools      	>", "My Favorites            	>", "Principal Blogs 	 	>", "Principal News      		>"};
+    String[] titles_str = {"My Notifications", "My Calendar", "Key Business Tools", "My Favorites", "Principal Blogs", "Principal News"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mydb = new DatabaseHelper(this);
-        addData();
+        //addData();
         String recent_token = FirebaseInstanceId.getInstance().getToken();
-        //Toast.makeText(activity_home.this, "recent_token: " + recent_token, Toast.LENGTH_LONG).show();
         Log.d(REG_TOKEN, recent_token);
 
         ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles_str);
@@ -56,6 +55,7 @@ public class activity_home extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(activity_home.this, NotificationListActivity.class);
                 String Items = String.valueOf(parent.getItemAtPosition(position));
+                //i.putExtra("Intent","activity_home");
                 if (Items == "My Notifications") {
                     startActivity(i);
                 } else {
@@ -72,58 +72,47 @@ public class activity_home extends AppCompatActivity {
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
                 if (key.equals("title")) {
-                    Toast.makeText(activity_home.this, getIntent().getExtras().getString(key), Toast.LENGTH_LONG).show();
                     title = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("message")) {
-                    Toast.makeText(activity_home.this, getIntent().getExtras().getString(key), Toast.LENGTH_LONG).show();
                     message = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("insured")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     insured = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("email")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     email = getIntent().getExtras().getString(key);
                 }
-                if (key.equals("phone")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
+                if (key.equals("phone_num")) {
                     phone = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("policy_num")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     policy_num = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("amount")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     amount = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("currency")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     currency = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("due_date")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     due_date = getIntent().getExtras().getString(key);
                 }
                 if (key.equals("notes")) {
-                    //Toast.makeText(activity_home.this,getIntent().getExtras().getString(key),Toast.LENGTH_LONG).show();
                     notes = getIntent().getExtras().getString(key);
                 }
             }
         }
         if (title != null && message != null && insured != null) {
-            //String tile, String message, String Insured, String email, String phone,String policy_num, Integer amount, String currency, String due_Date, String notes
             isPushNotificationSaved = mydb.insertData(title, message, insured, email, phone, policy_num, amount, currency, due_date, notes);
             if (isPushNotificationSaved = true) {
-                Toast.makeText(activity_home.this, "Push Notification Saved! Details : Title: " + title + " Message: " + message + " Insured: " + insured, Toast.LENGTH_LONG).show();
+                Toast.makeText(activity_home.this, "Notification Saved Successfully!", Toast.LENGTH_LONG).show();
             }
             if (isPushNotificationSaved != true) {
-                Toast.makeText(activity_home.this, "Push Notification Not Saved! Details : Title: " + title + " Message: " + message + " Insured: " + insured, Toast.LENGTH_LONG).show();
+                Toast.makeText(activity_home.this, "Notification not Saved Successfully.", Toast.LENGTH_LONG).show();
             }
         } else {
-            Toast.makeText(activity_home.this, "Push Notification Details : Title: " + title + " Message: " + message + " Insured: " + insured, Toast.LENGTH_LONG).show();
+            Toast.makeText(activity_home.this, "Title, Message and Insured details are missing.", Toast.LENGTH_LONG).show();
         }
         return isPushNotificationSaved;
     }
